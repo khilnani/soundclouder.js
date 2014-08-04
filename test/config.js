b75441e33abd877c044569afb9b6245e84839cdc
khilnani <nik@khilnani.org>
2014-08-04 00:30:16 -0400

    Updated Doc and version

---
+++ test/config.js
@@ -0,0 +1,7 @@
+var config = exports;
+
+config.sc_client_id =		"CLIENT_ID";
+config.sc_client_secret = 	"CLIENT_SECRET";
+config.sc_redirect_uri = 	"REDIRECT_URI";
+config.oauth_token = 		"OAUTH_TOKEN";
+
