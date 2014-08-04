859211ce4d79a45e4857cfb9513eaca8ec734fb9
Nik <nik@khilnani.org>
2014-08-03 22:48:37 -0400

    Update and rename config.js to config.sample.js

--- test/config.js
+++
@@ -1,8 +0,0 @@
-var config = exports;
-
-config.sc_client_id =		"290a09c2aab46faf30a3dd4965ded293";
-config.sc_client_secret = 	"06f96b5c05d1727df4024bd406dfe31f";
-config.sc_redirect_uri = 	"http://khilnani.github.io/soundclouder.js/sc.html";
-config.sc_code =			"7a6097283a8f3841db62d7ed6ad5e108";
-config.oauth_token = 		"1-88671-85370204-7ea11fc99b60396";
-
